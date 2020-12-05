c7e4787e76ed74fd4857da19cab52e0140eb385b
FanTom07 <fantomjr07@gmail.com>
2020-12-05 22:37:03 +0300

    Updates

---
+++ precache-manifest.8f26bdc06da773a1fb016cbaa887f812.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7e2c4637e0b8d400b5d2da828cb03dc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "80ed7c924ee5c4b9c340",
+    "url": "./static/css/2.cdee455b.chunk.css"
+  },
+  {
+    "revision": "86e07a9f0dd95545ccac",
+    "url": "./static/css/main.2e2fd249.chunk.css"
+  },
+  {
+    "revision": "80ed7c924ee5c4b9c340",
+    "url": "./static/js/2.5f92a9de.chunk.js"
+  },
+  {
+    "revision": "73aa6e5c2b67a9d3cb980f1d672eeff0",
+    "url": "./static/js/2.5f92a9de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86e07a9f0dd95545ccac",
+    "url": "./static/js/main.be186ef5.chunk.js"
+  },
+  {
+    "revision": "e86c2ff2f1eb78554e57",
+    "url": "./static/js/runtime-main.0660989e.js"
+  }
+]);
